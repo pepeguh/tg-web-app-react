52256b4505be624d6af6bbbd928aab9ea099c2a2
pepeguh <141973944+pepeguh@users.noreply.github.com>
2023-09-12 17:59:06 +1200

    upd3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import './App.css'
 import { useTelegram } from './hooks/useTelegram';
+import Header from './components/Header/Header';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 
   return (
     <div>
+      <Header/>
       <button onClick={onToggleButton}>toggle</button>
       
     </div>
